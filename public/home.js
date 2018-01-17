33a638a822bc1859f1713b56e2849ab72150c81e
Remi Caumette <remicaumette@icloud.com>
2018-01-17 22:29:50 +0100

    :lipstick: Design update

--- public/home.js
+++ public/home.js
@@ -1,47 +1,46 @@
 /* eslint-disable */
 
-moment.locale('fr');
-
 var eventSource = new EventSource('/events');
 
-var CHART_BY_SENSOR = {
-    TEMPERATURE: new Chart(document.getElementById('temperature-chart'), {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [
-                { label: 'Température', fill: false, borderColor: '#304FFE' },
-            ]
-        }
-    }),
-    CARBON_MONOXIDE: new Chart(document.getElementById('carbon-monoxide-chart'), {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [
-                { label: 'Monoxyde de carbone', fill: false, borderColor: '#304FFE' },
-            ]
-        }
-    }),
-    POWER_CONSUMPTION: new Chart(document.getElementById('power-consumption-chart'), {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [
-                { label: 'Consommation électrique', fill: false, borderColor: '#304FFE' },
-            ]
-        }
-    }),
-}
+var chart = new Chart(document.getElementById('chart'), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [
+            { label: 'Température', fill: false, borderColor: '#304FFE', lineTension: 0, borderWidth: 2 },
+            { label: 'Monoxyde de carbone', fill: false, borderColor: '#424242', lineTension: 0, borderWidth: 2 },
+            { label: 'Consommation électrique', fill: false, borderColor: '#00C853', lineTension: 0, borderWidth: 2 },
+        ],
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    displayFormats: {
+                        quarter: 'hh:mm'
+                    },
+                },
+            }],
+        },
+    },
+});
+
+var DATASET_INDEX_BY_SENSOR = {
+    TEMPERATURE: 0,
+    CARBON_MONOXIDE: 1,
+    POWER_CONSUMPTION: 2,
+};
+
+moment.locale('fr');
 
 eventSource.onmessage = function (event) {
     var dataArray = JSON.parse(event.data);
     dataArray = dataArray.length ? dataArray : [dataArray];
     console.log('Données reçus ('+ dataArray.length +')');
     dataArray.forEach(function (data) {
-        var chart = CHART_BY_SENSOR[data.sensor];
-        chart.data.labels.push(moment(data.receivedAt).calendar());
-        chart.data.datasets[0].data.push(data.value);
+        chart.data.labels.push(new Date(data.receivedAt));
+        chart.data.datasets[DATASET_INDEX_BY_SENSOR[data.sensor]].data.push({ x: new Date(data.receivedAt), y: data.value});
         chart.update();
     });
 };
