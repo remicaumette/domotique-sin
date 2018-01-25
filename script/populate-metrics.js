3808a4077f12171abd3155bfe357d047e32b1320
Remi Caumette <remicaumette@icloud.com>
2018-01-25 09:12:30 +0100

    :construction: Adding some functions on the database

--- script/populate-metrics.js
+++ script/populate-metrics.js
@@ -2,10 +2,14 @@ const Database = require('../lib/database');
 
 const redis = Database.openConnection();
 
-setInterval(() => {
+const populate = () => {
     redis.publish('metrics', JSON.stringify({ sensor: 'TEMPERATURE', value: Math.floor(Math.random() * 40) }));
     redis.publish('metrics', JSON.stringify({ sensor: 'CARBON_MONOXIDE', value: Math.floor(Math.random() * 30) }));
     redis.publish('metrics', JSON.stringify({ sensor: 'POWER_CONSUMPTION', value: Math.floor(Math.random() * 100) }));
 
     console.log('Populate!');
-}, 60 * 1000);
+};
+
+setInterval(populate, 60 * 1000);
+
+populate();
